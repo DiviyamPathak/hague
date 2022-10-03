7b587187cc88d7fd96b6440a171f35083d1bf19e
DiviyamPathak <patdiviyam@gmail.com>
2022-10-04 02:06:29 +0530

    done with working get and put

--- backend/controllers/excCon.js
+++ backend/controllers/excCon.js
@@ -1,24 +1,32 @@
-const excr = require('../dbModel.js');
+
+
+const Excr = require('../dbModel.js');
 const thencat = require('express-async-handler');
 
 const getHandler = thencat(async (req,res)=>{
-    const excer = excr.find();
+    const excer = await Excr.find();
 
     res.status(200).json(excer)
 })
 
 
 const postHandler = thencat(async (req,res)=>{
+    if (!req.body ){
+        res.status(400);
+        throw new Error('please add all the fields required');
+    }
     
+    const newexcercise = await Excr.create({desc:  req.body.desc, title : req.body.title,tag : req.body.tag});
+    res.send(200).json(newexcercise);
 
 })
 
 const putHandler = thencat(async (req,res)=>{
-
+        console.log('puthandler');
 })
 
 const deleteHandler = thencat(async (req,res)=>{
-
+    console.log('delhandler');
 })
 
-export default{getHandler ,postHandler, putHandler,deleteHandler}
+module.exports = {getHandler ,postHandler, putHandler,deleteHandler};
